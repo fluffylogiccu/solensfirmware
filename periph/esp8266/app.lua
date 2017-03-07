aefa85e4a0a14702f774878a9c1230b3112f136b
Ben Heberlein <ben.heberlein@gmail.com>
2017-03-06 21:57:17 -0700

    successfully sent TCP data from file

--- periph/esp8266/app.lua
+++ periph/esp8266/app.lua
@@ -15,31 +15,57 @@ q = {}
 
 sck=net.createConnection(net.TCP, 0)
 
-function sentCallback(sent)  
+--function sentCallback(sent)  
+--    print ("in sentCallback")
+--    d = file.read(128)
+--    if not d == nil then
+--        sck:send(d)
+--    end
 --    print ("entered sentCallback")
-    if not tableEmpty(q) then
+--    if not tableEmpty(q) then
 --        print("table has data, calling sck send")
-        sck:send(table.remove(q,1))
-    end
-end
-sck:on("sent", sentCallback)
+--        sck:send(table.remove(q,1))
+--    end
+--end
 sck:on("connection", function() 
     print ("Connected to TCP server on port "..PORT.." and host "..HOST)
     
     if not tmr.create():alarm(1000, tmr.ALARM_SINGLE, function()
-        uart.alt(1)
-        uart.setup(0, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1, 0)
-        uart.on("data", 128, function(data)
-            if tableEmpty(q) then
---                print ("table is empty, inserting data")
-                table.insert(q,d)
---                print ("calling sentCallback")
-                sentCallback('')
-            else
---                print ("table is not empty, inserting data")
-                table.insert(q,d)
+        print ("Fired timer")
+        if file.open("img.raw", "r") then
+            print ("opened file")
+            local function sendChunk()
+                local line = file.read(512)
+                if line then
+                    sck:send(line,sendChunk)
+                    print ("sent chunk")
+                else
+                    print("closing file")
+                    file.close()
+                    sck:close()
+                    collectgarbage()
+                end
             end
-        end, 0)
+            sendChunk()
+        else
+            print ("could not open file")
+        end
+        
+--        print ("calling sentCallback")        
+--        sentCallback('')
+--        uart.alt(1)
+--        uart.setup(0, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1, 0)
+--        uart.on("data", 128, function(data)
+--            if tableEmpty(q) then
+--                print ("table is empty, inserting data")
+--                table.insert(q,d)
+--                print ("calling sentCallback")
+--                sentCallback('')
+--            else
+--                print ("table is not empty, inserting data")
+--                table.insert(q,d)
+--            end
+--        end, 0)
     end)
     then
         print ("Could not create timer. UART was not initialized.")
