2921a61b9af5f8b188bf77ee1cf3480f55793f0a
Ben Heberlein <ben.heberlein@gmail.com>
2017-03-07 00:20:01 -0700

    TCP transfer with UART trigger, not correct data still

--- periph/esp8266/app.lua
+++ periph/esp8266/app.lua
@@ -15,57 +15,56 @@ q = {}
 
 sck=net.createConnection(net.TCP, 0)
 
---function sentCallback(sent)  
---    print ("in sentCallback")
---    d = file.read(128)
---    if not d == nil then
---        sck:send(d)
---    end
---    print ("entered sentCallback")
---    if not tableEmpty(q) then
---        print("table has data, calling sck send")
---        sck:send(table.remove(q,1))
---    end
---end
+current_size = 0
+total_size = 153472 --153728
+
+function sendFile()
+
+    if file.open("img.raw", "r") then
+        --print ("opened file")
+        local function sendChunk()
+            local line = file.read(512)
+            if line then
+                sck:send(line,sendChunk)
+                --print ("sent chunk")
+            else
+                --print("closing file")
+                file.close()
+                sck:close()
+                collectgarbage()
+            end
+        end
+        sendChunk()
+    else
+        --print ("could not open file")
+    end
+
+    current_size = 0
+    --file.remove("temp.raw")
+
+end
+
 sck:on("connection", function() 
     print ("Connected to TCP server on port "..PORT.." and host "..HOST)
     
     if not tmr.create():alarm(1000, tmr.ALARM_SINGLE, function()
-        print ("Fired timer")
-        if file.open("img.raw", "r") then
-            print ("opened file")
-            local function sendChunk()
-                local line = file.read(512)
-                if line then
-                    sck:send(line,sendChunk)
-                    print ("sent chunk")
-                else
-                    print("closing file")
-                    file.close()
-                    sck:close()
-                    collectgarbage()
+        file.remove("temp.raw")
+        file.open("temp.raw","w")
+        file.close()
+        current_size = 0
+
+        uart.alt(1)
+        uart.setup(0, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1, 0)
+        uart.on("data", 128, function(data)
+            if file.open("temp.raw", "a+") then
+                file.write(data)
+                file.close()
+                current_size = current_size + #data
+                if current_size >= total_size then
+                    sendFile()
                 end
             end
-            sendChunk()
-        else
-            print ("could not open file")
-        end
-        
---        print ("calling sentCallback")        
---        sentCallback('')
---        uart.alt(1)
---        uart.setup(0, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1, 0)
---        uart.on("data", 128, function(data)
---            if tableEmpty(q) then
---                print ("table is empty, inserting data")
---                table.insert(q,d)
---                print ("calling sentCallback")
---                sentCallback('')
---            else
---                print ("table is not empty, inserting data")
---                table.insert(q,d)
---            end
---        end, 0)
+        end, 0)
     end)
     then
         print ("Could not create timer. UART was not initialized.")
